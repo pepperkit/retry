4137f645747848bffee685579347b2d6130483e4
Artur Aukhatov <aaukhatov@gmail.com>
2021-03-02 22:43:03 +0300

    Add test for default exponential backoff function

--- src/test/java/io/github/pepperkit/retry/BackoffFunctionTest.java
+++ src/test/java/io/github/pepperkit/retry/BackoffFunctionTest.java
@@ -38,6 +38,20 @@ class BackoffFunctionTest {
     }
 
     @Test
+    void defaultExponentialBackoff() {
+        Duration initialDelay = Duration.ofSeconds(3);
+        BackoffFunction.Exponential function = new BackoffFunction.Exponential();
+
+        Duration delay1 = function.delay(1, initialDelay);
+        Duration delay2 = function.delay(2, initialDelay);
+        Duration delay3 = function.delay(3, initialDelay);
+
+        assertEquals(Duration.ofSeconds(9), delay1);
+        assertEquals(Duration.ofSeconds(27), delay2);
+        assertEquals(Duration.ofSeconds(81), delay3);
+    }
+
+    @Test
     void randomizedBackoff() {
         Duration initialDelay = Duration.ofSeconds(3);
         BackoffFunction.Randomized function = new BackoffFunction.Randomized(5);
