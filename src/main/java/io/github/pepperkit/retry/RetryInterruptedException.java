e142724ecc766141d3db4f2e10a75d87313050ea
Artur Aukhatov <aaukhatov@gmail.com>
2021-03-14 21:11:23 +0300

    Remove redundant constructors from RetryInterruptedException

--- src/main/java/io/github/pepperkit/retry/RetryInterruptedException.java
+++ src/main/java/io/github/pepperkit/retry/RetryInterruptedException.java
@@ -4,18 +4,6 @@ public class RetryInterruptedException extends RuntimeException {
 
     private static final long serialVersionUID = 5443040601324416580L;
 
-    public RetryInterruptedException() {
-        super();
-    }
-
-    public RetryInterruptedException(String message) {
-        super(message);
-    }
-
-    public RetryInterruptedException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
     public RetryInterruptedException(Throwable cause) {
         super(cause);
     }
