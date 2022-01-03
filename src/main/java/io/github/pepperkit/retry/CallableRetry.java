b767f562e52636776e8a23b6b10cca88f13f397e
Ilia Voloshanovskii <ilyavy.inno@gmail.com>
2022-01-03 18:24:55 +0400

    Update license

--- src/main/java/io/github/pepperkit/retry/CallableRetry.java
+++ src/main/java/io/github/pepperkit/retry/CallableRetry.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (C) 2022 PepperKit
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
 package io.github.pepperkit.retry;
 
 public interface CallableRetry<V> {
